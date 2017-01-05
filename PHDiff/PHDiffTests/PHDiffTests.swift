5053cd1ab93e1b2676eecdaee19ff175eb8a29d8
Evgeniy Yurtaev <evgeniyyurt@gmail.com>
2017-01-05 19:47:51 +0300

    Remove requirement to use sorted steps in apply steps method

--- PHDiff/PHDiffTests/PHDiffTests.swift
+++ PHDiff/PHDiffTests/PHDiffTests.swift
@@ -19,47 +19,47 @@ final class PHDiffTests: XCTestCase {
         oldArray = ["a", "b", "c"]
         newArray = ["a", "b", "c"]
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray)
+        XCTAssertTrue(try! oldArray.apply(steps: steps) == newArray)
 
         oldArray = []
         newArray = ["a", "b", "c", "d", "e"]
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray)
+        XCTAssertTrue(try! oldArray.apply(steps: steps) == newArray)
 
         oldArray = ["a", "b", "c", "c", "c"]
         newArray = []
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray)
+        XCTAssertTrue(try! oldArray.apply(steps: steps) == newArray)
 
         oldArray = ["a", "b", "c", "c", "c"]
         newArray = ["e", "b", "c", "d", "a"]
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray)
+        XCTAssertTrue(try! oldArray.apply(steps: steps) == newArray)
 
         oldArray = ["p", "U", "b", "A", "5", "F"]
         newArray = ["O", "w", "Z", "U"]
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray)
+        XCTAssertTrue(try! oldArray.apply(steps: steps) == newArray)
 
         oldArray = ["p", "b", "U", "A", "5", "F"]
         newArray = ["O", "w", "Z", "U"]
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray)
+        XCTAssertTrue(try! oldArray.apply(steps: steps) == newArray)
 
         oldArray = ["x", "E", "g", "B", "f", "o", "3", "m"]
         newArray = ["j", "f", "L", "L", "m", "V", "g", "Q", "1"]
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray)
+        XCTAssertTrue(try! oldArray.apply(steps: steps) == newArray)
 
         oldArray = ["j", "E", "g", "B", "f", "o", "3", "m"]
         newArray = ["j", "f", "L", "L", "m", "V", "g", "Q", "1"]
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray)
+        XCTAssertTrue(try! oldArray.apply(steps: steps) == newArray)
 
         oldArray = ["a", "b", "c", "c", "c"]
         newArray = ["e", "b", "c", "d", "a"]
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray)
+        XCTAssertTrue(try! oldArray.apply(steps: steps) == newArray)
 
         oldArray = ["a", "b", "c"]
         newArray = ["b", "c", "c"]
@@ -80,6 +80,92 @@ final class PHDiffTests: XCTestCase {
         ])
     }
 
+    func testApplyDiffWithBadInsertIndexSteps() {
+        let steps: [DiffStep<String>] = [
+            .insert(value: "d", index: 4),
+        ]
+        do {
+            let _ = try [ "a", "b", "c" ].apply(steps: steps)
+            XCTAssert(false)
+        } catch DiffError<String>.failApplyDiffStep(step: let step) {
+            XCTAssertEqual(step, DiffStep.insert(value: "d", index: 4))
+        } catch _ {
+            XCTAssert(false)
+        }
+    }
+
+    func testApplyDiffWithBadInsertIndexAfterRemoveSeveralSteps() {
+        let steps: [DiffStep<String>] = [
+            .delete(value: "a", index: 0),
+            .delete(value: "b", index: 1),
+            .insert(value: "c", index: 3),
+        ]
+        do {
+            let _ = try [ "a", "b", "c", "d" ].apply(steps: steps)
+            XCTAssert(false)
+        } catch DiffError<String>.failApplyDiffStep(step: let step) {
+            XCTAssertEqual(step, DiffStep.insert(value: "c", index: 3))
+        } catch _ {
+            XCTAssert(false)
+        }
+    }
+
+    func testRemoveItemOutRangeIndex() {
+        let steps: [DiffStep<String>] = [
+            .delete(value: "f", index: 4),
+        ]
+        do {
+            let _ = try [ "a", "b", "c", "d" ].apply(steps: steps)
+            XCTAssert(false)
+        } catch DiffError<String>.failApplyDiffStep(step: let step) {
+            XCTAssertEqual(step, DiffStep.delete(value: "f", index: 4))
+        } catch _ {
+            XCTAssert(false)
+        }
+    }
+
+    func testMoveWithInvalidFromIndex() {
+        let steps: [DiffStep<String>] = [
+            .move(value: "f", fromIndex: 4, toIndex: 0),
+        ]
+        do {
+            let _ = try [ "a", "b", "c", "d" ].apply(steps: steps)
+            XCTAssert(false)
+        } catch DiffError<String>.failApplyDiffStep(step: let step) {
+            XCTAssertEqual(step, DiffStep.move(value: "f", fromIndex: 4, toIndex: 0))
+        } catch _ {
+            XCTAssert(false)
+        }
+    }
+
+    func testMoveWithInvalidToIndex() {
+        let steps: [DiffStep<String>] = [
+            .move(value: "a", fromIndex: 0, toIndex: 4),
+            ]
+        do {
+            let _ = try [ "a", "b", "c", "d" ].apply(steps: steps)
+            XCTAssert(false)
+        } catch DiffError<String>.failApplyDiffStep(step: let step) {
+            XCTAssertEqual(step, DiffStep.insert(value: "a", index: 4))
+        } catch _ {
+            XCTAssert(false)
+        }
+    }
+
+    func testUpdateWithInvalidIndex() {
+        let steps: [DiffStep<String>] = [
+            .update(value: "a", index: 4),
+            ]
+        do {
+            let _ = try [ "a", "b", "c", "d" ].apply(steps: steps)
+            XCTAssert(false)
+        } catch DiffError<String>.failApplyDiffStep(step: let step) {
+            XCTAssertEqual(step, DiffStep.update(value: "a", index: 4))
+        } catch _ {
+            XCTAssert(false)
+        }
+    }
+
     func testDiffUpdate() {
         var oldArray: [TestUser] = []
         var newArray: [TestUser] = []
@@ -91,7 +177,7 @@ final class PHDiffTests: XCTestCase {
         steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
         expectedSteps = [.update(value: TestUser(name: "2", age: 1), index: 1)]
         XCTAssertTrue(steps == expectedSteps)
-        XCTAssertTrue(oldArray.apply(steps: steps) == newArray, "simple update")
+        XCTAssertEqual(try! oldArray.apply(steps: steps), newArray, "simple update")
     }
 
     func testRandomDiffs() {
@@ -103,14 +189,7 @@ final class PHDiffTests: XCTestCase {
             let newArray = randomArray(length: randomNumber(0..<500))
 
             let steps = PHDiff.sortedSteps(fromArray: oldArray, toArray: newArray)
-            let success = oldArray.apply(steps: steps) == newArray
-            XCTAssertTrue(success)
-
-            if !success {
-                print("oldArray = \(oldArray)")
-                print("newArray = \(newArray)")
-                break
-            }
+            XCTAssertEqual(try! oldArray.apply(steps: steps), newArray, "\noldArray = \(oldArray)\nnewArray = \(newArray)")
         }
     }
 
